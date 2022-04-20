a7d7a8d6f2856f635ab28cb0493752c81eb6c8b8
Igor Smolyakov <igrik82@gmail.com>
2022-04-20 15:56:21 +0500

    moved tests files to tests folder

--- Module_01/test_functions.py
+++
@@ -1,5 +0,0 @@
-import functions
-
-
-def test_hello():
-    assert functions.hello("Igor") == "Hello, Igor!"
--- Module_01/test_math_func.py
+++
@@ -1,7 +0,0 @@
-import math_func
-
-
-def test_add():
-    assert math_func.add(1, 2) == 3
-    assert math_func.add(2, 2) == 4
-    assert math_func.add(3, 2) == 5
