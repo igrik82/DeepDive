7a7c666b3175800c13651a2b38fc59493b98c17d
Igor Smolyakov <igrik82@gmail.com>
2022-04-20 15:55:52 +0500

    Added tests

---
+++ Module_01/tests/test_functions.py
@@ -0,0 +1,5 @@
+import functions
+
+
+def test_hello():
+    assert functions.hello('human') == 'Hello, human!'
---
+++ Module_01/tests/test_math_func.py
@@ -0,0 +1,7 @@
+from math_func import add
+
+
+def test_add():
+    assert add(1, 2) == 3
+    assert add(2, 2) == 4
+    assert add(3, 2) == 5
